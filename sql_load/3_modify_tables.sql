ddccbf5954687589e4de8ddf90134cde9d979c74
Hakan Aydin <hakan.aydin5234@gmail.com>
2024-05-04 22:55:53 +0200

    first commit

---
+++ sql_load/3_modify_tables.sql
@@ -0,0 +1,15 @@
+COPY company_dim
+FROM 'D:\Development\SQL_Project_Data_Job_Analysis\csv_files\company_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_dim
+FROM 'D:\Development\SQL_Project_Data_Job_Analysis\csv_files\skills_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY job_postings_fact
+FROM 'D:\Development\SQL_Project_Data_Job_Analysis\csv_files\job_postings_fact.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_job_dim
+FROM 'D:\Development\SQL_Project_Data_Job_Analysis\csv_files\skills_job_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
